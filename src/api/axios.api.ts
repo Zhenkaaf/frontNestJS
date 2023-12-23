92b140d06ba7cc9e5cefa14e9af0c9934e04d0f4
Zhenkaaf <zhenkaaf@gmail.com>
2023-12-23 20:24:57 +0200

    authService-localStor-toastify

---
+++ src/api/axios.api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import { getTokenFromLocalStorage } from '../helpers/localstorage.helper'
+export const instance = axios.create({
+	baseURL: 'http://localhost:8800/api',
+	headers: {
+		Authorization: `Bearer` + getTokenFromLocalStorage(),
+	},
+})
